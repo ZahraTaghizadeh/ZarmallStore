f80207238803dc5568a6fee3bd17dd23f7ccbc3c
z.taghizadeh <z.taghizadeh@TAGHIZADEHZ-PC>
2025-09-27 13:32:09 +0330

    edit kavenegar

--- ZarmallStore.Application/Services/Implementations/SmsService.cs
+++ ZarmallStore.Application/Services/Implementations/SmsService.cs
@@ -5,7 +5,7 @@ namespace ZarmallStore.Application.Services.Implementations
     public class SmsService : ISmsService
     {
         #region Api Key
-        private string apiKey = "6A75357657354656646453684F396541356635564E2B6770627A3541786159524C45585867643048304E6F3D";
+        private string apiKey = "6830636F2B31567A6A36547856574D50546449744E784A374C546874756F77565641377778435A6F6278303D";
         #endregion
 
         public async Task SendVerificationSms(string mobile, string code)
